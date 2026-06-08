7c499a4ca3e4092ac51b20343801b100575c9be3
Umitcan CİNAR <cinarx04@gmail.com>
2026-06-08 18:25:46 +0300

    canlı site içeriği eklendi

---
+++ canlı_site_icerigi/atys-app/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: './', // Dosya yollarını Vercel ve GitHub Pages için uyumlu hale getirir
+})
